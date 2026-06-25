91528064e45f49d966f2272c3e1b59e564ceb314
Jrabara101 <jrabara101@gmail.com>
2026-06-25 16:58:05 +0800

    Step 3: Write Initialize address

--- contracts/device_registry/src/lib.rs
+++ contracts/device_registry/src/lib.rs
@@ -1,5 +1,5 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, contracttype, Address, BytesN};
+use soroban_sdk::{contract, contractimpl, contracttype, contracterror, panic_with_error, Address, BytesN, Env};
 
 #[contracttype]
 pub enum DataKey {
@@ -7,8 +7,22 @@ pub enum DataKey {
     DeviceMap(BytesN<32>),
 }
 
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+#[repr(u32)]
+pub enum Error {
+    AlreadyInitialized = 1,
+}
+
 #[contract]
 pub struct DeviceRegistry;
 
 #[contractimpl]
-impl DeviceRegistry {}
+impl DeviceRegistry {
+    pub fn initialize(env: Env, admin: Address) {
+        if env.storage().instance().has(&DataKey::Admin) {
+            panic_with_error!(&env, Error::AlreadyInitialized);
+        }
+        env.storage().instance().set(&DataKey::Admin, &admin);
+    }
+}
