9194edfd34c887903ceaa207a2e5246eee78045d
Jrabara101 <jrabara101@gmail.com>
2026-06-26 14:15:54 +0800

    Step 4 and Step 5

--- contracts/device_registry/src/lib.rs
+++ contracts/device_registry/src/lib.rs
@@ -1,5 +1,5 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, contracttype, contracterror, panic_with_error, Address, BytesN, Env};
+use soroban_sdk::{contract, contractimpl, contracttype, contracterror, panic_with_error, symbol_short, Address, BytesN, Env};
 
 #[contracttype]
 pub enum DataKey {
@@ -12,6 +12,7 @@ pub enum DataKey {
 #[repr(u32)]
 pub enum Error {
     AlreadyInitialized = 1,
+    DeviceNotFound = 2,
 }
 
 #[contract]
@@ -24,5 +25,24 @@ impl DeviceRegistry {
             panic_with_error!(&env, Error::AlreadyInitialized);
         }
         env.storage().instance().set(&DataKey::Admin, &admin);
+    }
+
+    pub fn register(env: Env, device_hash: BytesN<32>, wallet: Address) {
+        let admin: Address = env.storage().instance().get(&DataKey::Admin).unwrap();
+        admin.require_auth();
+
+        env.storage()
+            .persistent()
+            .set(&DataKey::DeviceMap(device_hash.clone()), &wallet);
+
+        env.events()
+            .publish((symbol_short!("register"), device_hash), wallet);
+    }
+
+    pub fn get_wallet(env: Env, device_hash: BytesN<32>) -> Address {
+        env.storage()
+            .persistent()
+            .get(&DataKey::DeviceMap(device_hash))
+            .unwrap_or_else(|| panic_with_error!(&env, Error::DeviceNotFound))
     }
 }
